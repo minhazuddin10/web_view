4d9f65d5b2714580233b54b7dd9a3b9c4927cc18
minhaj uddin <minhaz112922@gmail.com>
2024-06-20 19:47:30 +0600

    add_final_push

--- app/src/main/java/com/example/web_view/MainActivity.java
+++ app/src/main/java/com/example/web_view/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.web_view;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        webView=findViewById(R.id.webId);
+        WebSettings webSettings =webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.youtube.com/watch?v=kf7no5jE8Jc");
+
     }
 }
